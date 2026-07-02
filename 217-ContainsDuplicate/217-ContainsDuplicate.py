7b27f233eeb332ac40b0ae17dfb77b69c33e953c
angus-maclean <angusmaclean2@gmail.com>
2026-07-02 20:20:27 +1000

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -0,0 +1,12 @@
+# Last updated: 02/07/2026, 20:20:26
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+      
+        hashset = set()
+
+        for num in nums:
+            if num in hashset:
+                return True
+            else:
+                hashset.add(num)
+        return False
